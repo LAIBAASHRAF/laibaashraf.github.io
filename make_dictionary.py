854012979b2ef18da96d5ce1f3b49b6136645c0a
LAIBA ASHRAF <112387588+LAIBAASHRAF@users.noreply.github.com>
2022-09-12 11:25:58 -0700

    Add files via upload

---
+++ make_dictionary.py
@@ -0,0 +1,5 @@
+dic= {"beautiful": "pretty", "mutable": "changeable", "immutable": "unchangeable", "set": "collection of ordered numbers"}
+user = input("enter any word to search  meaning for")
+print(dic)
+
+print(dic.get(user))
